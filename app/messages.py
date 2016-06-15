4be3041e425387bf28105001b10a7eef98c34355
Chris Penner <christopher.penner@gmail.com>
2016-06-14 20:16:50 -0600

    Add Mocking lib, extract messages as constants

---
+++ app/messages.py
@@ -0,0 +1,8 @@
+HOW_TO_START = "Text 'start CODE' with your story code to start and adventure!"
+STORY_NOT_FOUND = "Story not found"
+STARTING_NEW_STORY = "Starting the adventure! Your friends can text 'join {}' to join you."
+NO_GROUP_FOUND = "No group found for code {}"
+ALREADY_IN_GROUP = "You're already in that group"
+JOINED_GROUP = "You've joined the group! Here's the last message:"
+RESTARTED = "Restarted"
+END_OF_STORY = "Looks like you've hit the end of the story, text 'restart' to try again!"
--- test/test_scavenger.py
+++ test/test_scavenger.py
@@ -1,9 +1,17 @@
 from unittest import TestCase
 from webapp2 import Request
+from mock import Mock
 
 from google.appengine.ext import testbed, ndb
 
 from app.main import app
+
+from app.models.user import User
+from app.messages import HOW_TO_START, STORY_NOT_FOUND, STARTING_NEW_STORY, NO_GROUP_FOUND, \
+                     ALREADY_IN_GROUP, JOINED_GROUP, RESTARTED, END_OF_STORY
+from app.scavenger import CLUE, HINT, START_STORY, JOIN_GROUP, RESTART, ANSWER
+
+from app.scavenger import twiml_response
 
 
 class TestScavenger(TestCase):
@@ -33,7 +41,23 @@ class TestScavenger(TestCase):
         response = self.request.get_response(app)
         self.assertEqual(400, response.status_int)
 
-    def test_prompts_for_signup_when_no_user(self):
-        response = self.request.get_response(app)
-        self.assertEqual(200, response.status_int)
-        self.assertIn("don't recognize", response.body)
+class TestTwimlResponse(TestCase):
+    def setUp(self):
+        self.user = Mock(phone='+1111')
+
+    def test_includes_messages_in_response(self):
+        messages = ['HELLO!!', 'Hey there!']
+        response = twiml_response(self.user, START_STORY, messages)
+        for m in messages:
+            self.assertIn(m, response)
+
+    def test_sends_copies_to_each_recipient_for_CLUE_or_HINT(self):
+        phones = ['+1234', '+555', '+678']
+        self.user.group.users = phones
+        messages = ['HELLO!!', 'Hey there!']
+        response = twiml_response(self.user, CLUE, messages)
+        for p in phones:
+            self.assertIn(p, response)
+        response = twiml_response(self.user, HINT, messages)
+        for p in phones:
+            self.assertIn(p, response)
