6208e84ed573a598b80ce7422a0d9d4191a53939
Chris Penner <christopher.penner@gmail.com>
2016-07-26 18:05:52 -0600

    Add redux

--- client/app.jsx
+++ client/app.jsx
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom'
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './reducers'
 
 import { Stories, Story } from './story'
 import { Clue } from './clue'
 import Routes from './routes'
+
+const store = createStore(reducer)
 
 const App = ({children}) => (
     <div className="container"> {children} </div>
@@ -20,17 +25,19 @@ const My404 = () => (
 )
 
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <IndexRoute component={Index}/>
-            <Route path={Routes.stories()}>
-                <IndexRoute component={Stories}/>
-                <Route path=':storyUID' component={Story}/>
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Index}/>
+                <Route path={Routes.stories()}>
+                    <IndexRoute component={Stories}/>
+                    <Route path=':storyUID' component={Story}/>
+                </Route>
+                <Route path='/clues/:clueUID' component={Clue}/>
+                <Route path="*" component={My404}></Route>
             </Route>
-            <Route path='/clues/:clueUID' component={Clue}/>
-            <Route path="*" component={My404}></Route>
-        </Route>
-    </Router>,
+        </Router>
+    </Provider>,
     document.getElementById('app')
 )
 
