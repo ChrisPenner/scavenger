aa52eed76c77cd24db61d2c49ab16598a63307b4
Chris Penner <christopher.penner@gmail.com>
2016-09-19 21:49:00 -0600

    Allow custom serializers for ndb models.

--- server/app/models/group.py
+++ server/app/models/group.py
@@ -2,10 +2,13 @@ from uuid import uuid4
 
 from google.appengine.ext import ndb
 
+from webapp2_extensions import serialize_key
+
 from app.models.clue import Clue
 
-
 class Group(ndb.Model):
+    SERIALIZERS=[serialize_key]
+
     uid = ndb.StringProperty(required=True)
     data = ndb.JsonProperty(default={})
     story_uid = ndb.StringProperty(required=True)
