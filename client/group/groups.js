47c1e058592aac34d6ad84e3bc7881525421dff5
Adam Bissonnette <adam@mediamanifesto.com>
2016-09-19 21:51:17 -0600

    Built out groups ui

--- client/group/groups.js
+++ client/group/groups.js
@@ -11,19 +11,52 @@ const stateToProps = (state) => ({
 })
 
 const Groups = ({groupList}) => {
-  const groups = groupList.map(group => (
-    <Link
-      key={group.uid}
-      className="my-list-item">
-      {group.uid}
-    </Link>
-  ))
+  const groups = groupList.map(group => {
+      return (
+            <tr key={group.uid}>
+                <td>{group.uid}</td>
+                <td>{group.storyUid}</td>
+                <td>{group.clueUid}</td>
+                <td>{group.dateStarted}</td>
+                <td>{group.dateCompleted}</td>
+                <td>{group.hintsUsed}</td>
+                <td>{group.smsSent}</td>
+                <td>{group.mmsSent}</td>
+                <td>{group.smsReceived}</td>
+                <td>{group.mmsReceived}</td>
+                <td>
+                  <a className="button is-small is-primary">View Transcript</a>
+                  <a className="button is-small is-success">Restart Group</a>
+                  <a className="button is-small is-danger">End Group</a>
+                </td>
+            </tr>
+          )
+  })
 
   return (
     <div>
       <h1 className="title">Groups</h1>
       <div className="my-list">
-          aaa{groups}aaa
+          <table className="table is-bordered is-striped group__table">
+          <thead>
+            <tr>
+              <th>Group Code</th>
+              <th>Story Code</th>
+              <th>Current Clue</th>
+              <th>Date Started</th>
+              <th>Date Completed</th>
+              <th>Hints Used</th>
+              <th>SMS Sent</th>
+              <th>MMS Sent</th>
+              <th>SMS Received</th>
+              <th>MMS Received</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {groups}
+          </tbody>
+          </table>
         </div>
     </div>
   )
