3ba950e6599d9b32571c3eaddfbda84536638233
Adam Bissonnette <adam@mediamanifesto.com>
2016-09-24 12:43:44 -0600

    Split group / story markup into consts

--- client/message/index.js
+++ client/message/index.js
@@ -11,25 +11,35 @@ const stateToProps = state => ({
   storyUids: getStoryUids(state),
 })
 
-export const MessageChoices = connect(stateToProps)(({groupUids, storyUids}) => (
-  <section className="section">
-    <section className="columns">
-      <div className="column my-list">
-        <h1 className="title"> Groups </h1>
-        {groupUids.map(groupUid => <Link
-          to={Routes.groupMessages(groupUid)}
-          className="my-list-item"> 
-          {groupUid}
-        </Link>)}
-      </div>
-      <div className="column my-list">
-        <h1 className="title"> Stories </h1>
-        {storyUids.map(storyUid => <Link
-          to={Routes.storyMessages(storyUid)}
-          className="my-list-item"> 
-          {storyUid}
-        </Link>)}
-      </div>
+export const MessageChoices = connect(stateToProps)(({groupUids, storyUids}) => {
+  const groupList = groupUids.map(groupUid => (
+        <div className="my-list-item">
+          <Link
+            to={Routes.groupMessages(groupUid)}> 
+            {groupUid}
+          </Link>
+        </div>
+    ))
+  const storyList = storyUids.map(storyUid => (
+        <div className="my-list-item">
+          <Link
+            to={Routes.storyMessages(storyUid)}> 
+            {storyUid}
+          </Link>
+        </div>
+    ))
+  return (
+    <section className="section">
+      <section className="columns">
+        <div className="column my-list">
+          <h1 className="title"> Groups </h1>
+          {groupList}
+        </div>
+        <div className="column my-list">
+          <h1 className="title"> Stories </h1>
+          {storyList}
+        </div>
+      </section>
     </section>
-  </section>
-))
+  )}
+)
