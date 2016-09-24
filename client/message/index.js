63ef15a91819c23e6303cf967174da659ea2881a
Adam Bissonnette <adam@mediamanifesto.com>
2016-09-24 13:26:22 -0600

    Moved group table UI into messages/index

--- client/message/index.js
+++ client/message/index.js
@@ -3,23 +3,26 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
 import * as Routes from '../routes'
-import { getGroupUids, getStoryUids } from '../reducers'
+import { getGroupUids, getStoryUids, getGroupsList } from '../reducers'
 export {GroupMessages, StoryMessages} from './messages.js'
 
 const stateToProps = state => ({
-  groupUids: getGroupUids(state),
   storyUids: getStoryUids(state),
+  groupList: getGroupsList(state)
 })
 
-export const MessageChoices = connect(stateToProps)(({groupUids, storyUids}) => {
-  const groupList = groupUids.map(groupUid => (
-        <div className="my-list-item" key={groupUid}>
-          <Link
-            to={Routes.groupMessages(groupUid)}> 
-            {groupUid}
-          </Link>
-        </div>
-    ))
+export const MessageChoices = connect(stateToProps)(({groupList, storyUids}) => {
+  const groups = groupList.map(group => {
+      return (
+            <tr key={group.uid}>
+              <td><Link to={Routes.groupMessages(group.uid)}>{group.uid}</Link></td>
+                <td><Link to={Routes.storyMessages(group.storyUid)}>{group.storyUid}</Link></td>
+                <td>{group.clueUid}</td>
+                <td>{group.createdAt}</td>
+                <td>{group.completedAt}</td>
+            </tr>
+          )
+  })
   const storyList = storyUids.map(storyUid => (
         <div className="my-list-item" key={storyUid}>
           <Link
@@ -32,12 +35,26 @@ export const MessageChoices = connect(stateToProps)(({groupUids, storyUids}) => 
     <section className="section">
       <section className="columns">
         <div className="column my-list">
-          <h1 className="title"> Groups </h1>
-          {groupList}
-        </div>
-        <div className="column my-list">
           <h1 className="title"> Stories </h1>
           {storyList}
+        </div>
+        <div className="column is-three-quarters my-list">
+          <h1 className="title"> Groups </h1>
+
+          <table className="table is-bordered is-striped group__table">
+            <thead>
+              <tr>
+                <th>Group Code</th>
+                <th>Story Code</th>
+                <th>Current Clue</th>
+                <th>Date Started</th>
+                <th>Date Completed</th>
+              </tr>
+            </thead>
+            <tbody>
+              {groups}
+            </tbody>
+          </table>
         </div>
       </section>
     </section>
