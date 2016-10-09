9aa09328b8fdf7d0b4527f98183f42c1da42a7be
Chris Penner <christopher.penner@gmail.com>
2016-10-09 15:41:46 -0600

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+  "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+      "flowtype",
+      "react",
+      "mocha"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
